5d33cd3e5074bb95f7d04c313146f037ff45c58a
BennoAlif <bennoalif41@gmail.com>
2019-09-25 16:55:34 +0700

    Initial commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,19 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : Mendeklarasikan variabel teks
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newLine = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newLine + "Char:" + tab + "berisi " + c);
+    }
+}
